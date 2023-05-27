4acc7bf1c2a59a7ae3814dbbaeb9fc4809b06332
vjks <vjks@users.noreply.github.com>
2023-05-27 13:11:33 +0530

    Update DISCUS.php
    Removed the elseif statement.

--- code-chef-php/DISCUS.php
+++ code-chef-php/DISCUS.php
@@ -7,9 +7,9 @@ for($i = 0; $i < $tests; $i++) {
     $longest_distance = $distance1;
     if($distance1 < $distance2)
         $longest_distance = $distance2;
-    elseif ($distance2 < $distance3)
+    
+    if($distance1 < $distance3)
         $longest_distance = $distance3;
-    elseif($distance3 < $distance1)
-        $longest_distance = $distance1;
+    
     print($longest_distance . PHP_EOL);
 }
